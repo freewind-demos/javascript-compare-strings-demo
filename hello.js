090187533c81ba879bacb4fcc0f61d5331eed1b0
freewind <nowindlee@gmail.com>
2018-08-27 12:08:46 +0800

    demo

--- hello.js
+++ hello.js
@@ -1,1 +1,20 @@
-console.log('Hello, NodeJS')
+function compare(x, y) {
+    if (x < y) {
+        console.log(x + ' < ' + y)
+    } else if (x > y) {
+        console.log(x + ' > ' + y)
+    } else {
+        console.log(x + ' = ' + y)
+    }
+}
+
+compare('aaa', 'aaa')
+compare('aaa', 'aa')
+compare('aaa', 'aaaaa')
+compare('aaa', 'aab')
+compare('aaa', 'baa')
+compare('aaa', 'aa0')
+compare('aaa', 'Aaa')
+
+const array = ['aaa', 'aa', 'aaaaa', 'aab', 'aa0', 'Aaa', 'baa']
+console.log(array.sort())
---
+++ hello.js.md
@@ -0,0 +1,10 @@
+```
+aaa = aaa
+aaa > aa
+aaa < aaaaa
+aaa < aab
+aaa < baa
+aaa > aa0
+aaa > Aaa
+[ 'Aaa', 'aa', 'aa0', 'aaa', 'aaaaa', 'aab', 'baa' ]
+```
